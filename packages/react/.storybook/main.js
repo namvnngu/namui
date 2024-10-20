ca4cee28399263c17b5320e339b6a663db9197e5
Nam Nguyen <nguyenvietnam2401@gmail.com>
2024-10-20 23:47:39 +1100

    chore: improve react storybook setup

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -1,14 +1,13 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   stories: [
-    "../**/src/**/*.mdx",
-    "../**/src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
     "@storybook/addon-interactions",
   ],
   framework: {
